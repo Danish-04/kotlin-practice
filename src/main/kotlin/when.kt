d3040b44fee9770a34546fbb19824c62bdbe515e
danish <danishmuhammad118@gmail.com>
2023-10-05 12:36:56 +0530

    use of when

---
+++ src/main/kotlin/when.kt
@@ -0,0 +1,11 @@
+fun main(){
+   val student="Aman"
+
+    when(student){
+        "Aman" -> println("Student is Aman")
+        "Deepak" -> println("Student is Deepak")
+        "Sidhant" -> println("Student is Sidhant")
+        else -> println("Student is Danish")
+    }
+
+}
